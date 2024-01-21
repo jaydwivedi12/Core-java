d374d5cdcb8bb37c0c22e2c9e3f2d9ff83db7e7b
jaydwivedi12 <jayprakashdwivedi12@gmail.com>
2024-01-22 00:13:07 +0530

    test

--- helloworld/src/com/tns/helloworld/HelloWorld.java
+++ helloworld/src/com/tns/helloworld/HelloWorld.java
@@ -2,6 +2,10 @@ package com.tns.helloworld;
 
 public class HelloWorld {
 public static void main(String args[]) {
+<<<<<<< HEAD
 	System.out.println("Hello World");
+=======
+	System.out.println("Practice mode ");
+>>>>>>> 792043b (practice)
 }
 }
